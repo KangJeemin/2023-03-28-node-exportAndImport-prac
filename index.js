e2e01d478ca128f7d02d27c88eb05f074ff1dfc3
KangJeemin <wlals1234560@gmail.com>
2023-03-28 15:40:30 +0900

    export import 연습

---
+++ index.js
@@ -0,0 +1,5 @@
+import {msg} from './message.js'
+import * as msg2 from './message.js'
+
+console.log(msg);
+console.log(msg2.msg);
---
+++ message_object.js
@@ -0,0 +1,9 @@
+const obj = {
+  name:"강지민",
+  age:17,
+  func:function(name){
+    console.log(name);
+  }
+}
+
+export default obj
